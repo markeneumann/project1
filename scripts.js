cbcfeb5800cdfdd256c2011b94600a96ae9603f8
Mark Neumann <mark.neumann@westrock.com>
2019-10-20 12:53:25 -0500

    Add javascript to compute average

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
-// Enter a description of this program here.
+// Handle input and compute average
 
-/**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
- */
+const calculator = {
+  avg(a,b,c) {
+   return (Number(a) + Number(b)+ Number(c)) / 3;
+ }
+};
+
+const form=document.querySelector("form");
+const num1=document.querySelector("#game-1");
+const num2=document.querySelector("#game-2");
+const num3=document.querySelector("#game-3");
+const output=document.querySelector("output");
+
+form.addEventListener("submit", function(event){
+
+  event.preventDefault();
+
+  output.textContent = calculator["avg"]( num1.value, num2.value, num3.value);
+});
+
+
+
